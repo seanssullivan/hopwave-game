2bcd8ec9c6e048e8c7bd76ef7ad2fbb2c7b67620
Sean Sullivan <dev@seanmedia.ca>
2020-06-05 09:47:35 -0600

    Refactor App into components folder

--- src/hooks/useMusic.js
+++ src/hooks/useMusic.js
@@ -4,7 +4,7 @@ import { useState, useReducer } from "react";
  * useMusic is a hook to manage music playback with Tone.js
  */
 export function useMusic() {
-  const [ bpm, setBpm ] = useState(100);
+  const [bpm, setBpm] = useState(100);
 
   return { bpm, setBpm };
 }
