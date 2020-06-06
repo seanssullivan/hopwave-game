43a90a77d7e2d901af2c5828ec76eee3712a21df
Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
2020-06-05 19:40:29 -0600

    Add left and right movement using keyboard (#3)

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import { useFrame } from "react-three-fiber";
 
+import { useKeyPress } from "../hooks/useKeyPress";
+
 const CAR_WIDTH = 20;
 const CAR_HEIGHT = 5;
 const CAR_LENGTH = 20;
@@ -9,11 +11,17 @@ const CAR_COLOR = "white";
 export default function Car(props) {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
+  const { keyPressed: aKeyPressed } = useKeyPress("a");
+  const { keyPressed: dKeyPressed } = useKeyPress("d");
 
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
-    // mesh.current.position.z;
-    // Check where the car is on the road
+    if (aKeyPressed) {
+      mesh.current.position.x += 0.5;
+    }
+    if (dKeyPressed) {
+      mesh.current.position.x -= 0.5;
+    }
   });
 
   return (
