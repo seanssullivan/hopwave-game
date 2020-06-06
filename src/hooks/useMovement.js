2ea6ff3a7742c070e73b064b8eee266e0528370c
Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
2020-06-06 12:32:18 -0600

    Refactor road movement into custom hook (#6)
    * Implement moving road component
    
    * Refactor road component into individual segments
    
    * Refactor Car and Road components
    
    * Refactor movement into its own hook
    
    * Write docstring for useReposition hook

---
+++ src/hooks/useMovement.js
@@ -0,0 +1,13 @@
+// src/hooks/useMovement.js
+
+/**
+ * Return a function to move an object a provided distance.
+ * @param {Object} mesh - Three.js object mesh
+ * @param {String} axis - The axis on which to move the object (x, y, or z)
+ */
+export default function useMovement(mesh, axis) {
+  const setMovement = (distance) => {
+    mesh.current.position[axis] -= distance;
+  };
+  return setMovement;
+}
