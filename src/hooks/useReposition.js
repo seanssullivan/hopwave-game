7badac027ac8be8cacc825a2b8d62dca1251b22c
Sean Sullivan <dev@seanmedia.ca>
2020-06-06 11:44:10 -0600

    Write docstring for useReposition hook

--- src/hooks/useReposition.js
+++ src/hooks/useReposition.js
@@ -1,5 +1,9 @@
 // src/hooks/useReposition.js
 
+/**
+ * Return a function that repositions an object at new x, y and z coordinates.
+ * @param {Object} mesh - Three.js object mesh
+ */
 export default function useReposition(mesh) {
   const reposition = ({ x = 0, y = 0, z = 0 }) => {
     mesh.current.position.x = x;
