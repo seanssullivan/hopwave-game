c009a9f35282a1f28169a202734909299c908ae0
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-06-07 14:55:11 -0700

    added randomized shapes and moved randomobstacle

--- src/components/Shapes/Dodecahedron.jsx
+++ src/components/Shapes/Dodecahedron.jsx
@@ -2,35 +2,33 @@ import React, { useRef } from "react";
 import { useFrame } from "react-three-fiber";
 
 // Import hooks
-import useMovement from "../hooks/useMovement";
+import useMovement from "../../hooks/useMovement";
 
 // Import settings
-import settings from "../settings";
+import settings from "../../settings";
+
 const { SPEED } = settings.GAME;
 
-const SHAPE_WIDTH = 30;
+const RADIUS = 7;
 
-export default function RandomObstacle(props) {
+export default function Dodecahedron(props) {
   const { destroyObstacle } = props;
 
   // This reference will give us direct access to the mesh
   const mesh = useRef();
   const move = useMovement(mesh, "z");
-  
 
   useFrame(() => {
     move(0 - SPEED);
     if (mesh.current.position.z <= -200) {
-      
-      destroyObstacle()
+      destroyObstacle();
     }
   });
-  
+
   return (
     <mesh {...props} ref={mesh} scale={[1, 1, 1]}>
-      <boxBufferGeometry attach="geometry" args={[SHAPE_WIDTH, SHAPE_WIDTH, 1]} />
-      <meshStandardMaterial attach="material" color={'orange'} />
+      <dodecahedronBufferGeometry attach="geometry" args={[RADIUS]} />
+      <meshToonMaterial attach="material" color={"blue"} />
     </mesh>
-  )
-
+  );
 }
