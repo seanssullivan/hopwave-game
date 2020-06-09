be4312047122fec1e2c06d8a7b0581da6e91d26a
Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
2020-06-09 13:24:21 -0600

    Feature/use music (#24)
    * Add sounds to public
    
    * Get music to play

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import React, { useState, Suspense, useCallback, useEffect } from "react";
 import { Canvas } from "react-three-fiber";
+import * as Tone from "tone";
 import "./Game.scss";
 
 // Import components
@@ -9,16 +10,17 @@ import Grid from "./Grid";
 import Road from "./Road";
 import Car from "./Car";
 import Obstacles from "./Obstacles";
-import Sun from "./Sun";
-import Hud from "./Hud";
+// import Sun from "./Sun";
+// import Hud from "./Hud";
 import PalmTrees from "./PalmTrees";
-import Song from "./Song";
 
 // Import hooks
+import useMusic from "../hooks/useMusic";
+import useSoundEffects from "../hooks/useSoundEffects";
 import usePlayerPosition from "../hooks/usePlayerPosition";
 
 // Optional components
-import OrbitControl from "./OrbitControls";
+// import OrbitControl from "./OrbitControls";
 // import Zuckerberg from "./Zuckerberg";
 
 // Import settings
@@ -28,9 +30,12 @@ const { SPEED, START_POSITION } = settings.GAME;
 export default function Game() {
   const [playerPosition, setPlayerPosition] = usePlayerPosition(START_POSITION);
   const [speed, setSpeed] = useState(SPEED);
-  const [objectPositions, setObjectPositions] = useState({});
+  const [musicPlayer] = useMusic(speed);
+  const [playSound] = useSoundEffects();
 
-  useEffect(() => {});
+  useEffect(() => {
+    musicPlayer.playbackRate = 1 + speed / 10 - 5;
+  }, [musicPlayer, speed]);
 
   return (
     <>
@@ -41,12 +46,8 @@ export default function Game() {
         <Ground position={[0, -1, 200]} />
         <Road speed={speed} />
 
-        <Obstacles
-          playerPosition={playerPosition}
-          shapePositions={objectPositions}
-          setShapePositions={setObjectPositions}
-        />
-        <Sun />
+        <Obstacles soundEffect={playSound} playerPosition={playerPosition} />
+        {/* <Sun /> */}
         <Car
           color={"white"}
           avgSpeed={SPEED}
@@ -55,13 +56,13 @@ export default function Game() {
           setPosition={setPlayerPosition}
         />
 
-        <OrbitControl />
+        {/* <OrbitControl /> */}
         <Suspense fallback={null}>
           <PalmTrees />
           {/* <Zuckerberg/> */}
         </Suspense>
       </Canvas>
-      <Hud />
+      {/* <Hud player={player} /> */}
     </>
   );
 }
