8965c47bd6fb4a3ddb7884590c02ce02c91db8bd
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-06-13 13:15:14 -0700

    Josh feature (#40)
    * added id to roadsegment.  looked at adding a dipose function the the mesh...need to test it
    
    * deleted react logos and photos
    
    * added a zuckerberg
    
    * running zuckerberg add to game
    
    * added a gitfile loading in a docs folder
    
    * working on deleting things. nothing entirelyt figured out yet
    
    * Co-authored-by: Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
    
    Added obstacles to the course.
    
    * added randomized shapes and moved randomobstacle
    
    * cube playing music lol
    
    * added a hud with a song toggle
    
    * added hud and sun
    
    * deleted song
    song was just a duplicate of sun so just deleted it
    
    * fixed up the car a bit and the hud
    
    * working on adding a sprite...needs revision
    
    * stopped working on background coulndt get it to work
    
    * added zuckerberg main char. working on road
    
    * added a background image vaporwave style
    
    * trying to add context to the game
    
    * added a counter for points on the HUD
    
    * music on
    
    * added a hopwave logo to the game. click start to play the game
    
    * added a difficulty settings on the side
    
    * difficulty speed now changes how fast the shapes come up
    
    * added effects to the loading page
    
    * did some cleanup on the components
    
    * road is now a neon blue with a bity of transparency
    
    * added conditional rendering to dififculty and points
    
    tuned up the logo
    
    * cleanedup code base.
    added a new folder for 3dmodels not in use
    but difficulty speed in settings
    
    * deleted game scss not in use
    
    * more clean up
    
    * added an iframe component
    
    * making a loader
    
    Co-authored-by: Sean Sullivan <43550769+seanssullivan@users.noreply.github.com>
    Co-authored-by: Thea Ganden <60718472+Theag9998@users.noreply.github.com>

--- src/components/Iframe.jsx
+++ src/components/Iframe.jsx
@@ -1,33 +1,37 @@
-import React from "react";
-import { CircularProgress } from "@material-ui/core";
 
-export default class Iframe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
-  hideSpinner = () => {
-    this.setState({
-      loading: false,
-    });
-  };
-  render() {
-    return (
-      <div className="container rsvp-wrapper">
-        {this.state.loading ? (
-          <CircularProgress size={180} color="red" />
-        ) : null}
-        <iframe
-          title="Spotify"
-          src="https://open.spotify.com/embed/playlist/3PPbbsJhktmX5Cp6syx7gR"
-          frameborder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-          onLoad={this.hideSpinner}
-        />
+import React, { useState } from "react";
+import { CircularProgress } from "@material-ui/core";
+import "./Iframe.scss";
+
+export default function Iframe() {
+  let [loading, setLoading] = useState(true);
+
+  // returns a boolean
+  // let isIframeLoaded = function () {
+  //   let iframe = document.getElementById("iframe");
+  //   let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+  //   return iframeDoc.readyState === "complete";
+  // };
+
+  return (
+    <>
+      <div className={"outer"}>
+        {/* <div id={"circular-progress"} className={"inner"}>
+          {loading ? <CircularProgress color={"red"} size={180} /> : null}
+        </div> */}
+        <div id={"spotify-iframe"} className={"inner"}>
+          <iframe
+            display={!loading ? "block" : "none"}
+            id={"iframe"}
+            title="Spotify"
+            src="https://open.spotify.com/embed/playlist/3PPbbsJhktmX5Cp6syx7gR"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+            onLoad={() => setLoading(false)}
+          />
+        </div>
       </div>
-    );
-  }
+    </>
+  );
 }
